683ee5d355ce8c6ce1615ef6082c07367217099d
SueLetty <xiuxiang1129@gmail.com>
2022-09-20 10:07:52 -0700

    Added initialization in Wolf and Dog class.

--- src/main/java/com/tlglearning/animals/Dog.java
+++ src/main/java/com/tlglearning/animals/Dog.java
@@ -1,6 +1,36 @@
 package com.tlglearning.animals;
 
-public class Dog extends Wolf{
+public class Dog extends Wolf {
+
+  private static final int MY_CONSTANT = initializeValue();
+  private final int data = initializeData();
+
+  static {
+    System.out.println("Dog initialization block");
+  }
+
+  {
+    System.out.println("Dog instance initialization block");
+  }
+
+  {
+    System.out.println("Second dog initialization block");
+  }
+
+  private static int initializeValue() {
+    System.out.println("Dog.initializeValue");
+    return -2;
+  }
+
+  private int initializeData() {
+    System.out.println("Dog.initializeData");
+    return 2;
+  }
+
+  public Dog() {
+    super();
+    System.out.println("Dog constructor");
+  }
 
   @Override
   public void vocalize() {
@@ -11,10 +41,12 @@ public class Dog extends Wolf{
   public void hunt() {
     System.out.println("Hunt for balls, bones, leaves, etc.");
   }
-  public static void whoAmI(){
+
+  public static void whoAmI() {
     System.out.println("I am a dog.");
   }
-  public void rollOver(){
+
+  public void rollOver() {
     System.out.println("I'm rolling over! I'm the best dog!");
   }
 }
--- src/main/java/com/tlglearning/animals/Wolf.java
+++ src/main/java/com/tlglearning/animals/Wolf.java
@@ -1,13 +1,43 @@
 package com.tlglearning.animals;
 
 public class Wolf {
-  public void vocalize(){
+
+  private static final int MY_CONSTANT = initializeValue();
+
+  private final int data = initializeData();
+
+  static {
+    System.out.println("Wolf initialization block");
+  }
+
+  {
+    System.out.println("Wolf instance initialization block");
+  }
+
+  public Wolf() {
+    System.out.println("Wolf constructor");
+  }
+
+  private static int initializeValue() {
+    System.out.println("Wolf.initializeValue");
+    return -1;
+  }
+
+  private int initializeData() {
+    System.out.println("Wolf.initializeData");
+    return 1;
+  }
+
+
+  public void vocalize() {
     System.out.println("Howl like a wolf!");
   }
-  public void hunt(){
+
+  public void hunt() {
     System.out.println("Hunt in packs for live prey");
   }
-  public static void whoAmI(){
+
+  public static void whoAmI() {
     System.out.println("I am a wolf.");
   }
 }
